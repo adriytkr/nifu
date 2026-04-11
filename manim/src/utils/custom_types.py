e8599e56f6bc76d72410bfcfa3448e9ac684325e
Adriano <adriytkr@gmail.com>
2026-04-11 13:32:57 -0300

    update README

--- manim/src/utils/custom_types.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable
-
-import numpy as np
-import numpy.typing as npt
-
-scalarFunc=Callable[[float],float]
-coordinates=npt.NDArray[np.float64]
--- manim/src/utils/theme.py
+++
@@ -1,5 +0,0 @@
-from manim import ManimColor
-
-NEUTRAL_COLOR=ManimColor('#00ff00')
-HIGHLIGHT_COLOR=ManimColor("#FF0000")
-HIGHLIGHT_COLOR2=ManimColor("#0000ff")
