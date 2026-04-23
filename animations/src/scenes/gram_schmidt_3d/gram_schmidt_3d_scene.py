5beb8dbb8ce399143f245bb2c2271a1e17351f70
Adriano <adriytkr@gmail.com>
2026-04-23 19:40:02 -0300

    gram-schmidt-3d

--- animations/src/scenes/gram_schmidt_3d/gram_schmidt_3d_scene.py
+++ animations/src/scenes/gram_schmidt_3d/gram_schmidt_3d_scene.py
@@ -1,19 +1,52 @@
 from manim import *
 
-from src.scenes.gram_schmidt_3d.gram_schmidt_3d_scene_assets import GramSchmidt3DSceneAssets
+from src.scenes.gram_schmidt_3d.gram_schmidt_3d_scene_assets import GramSchmidt3DAssets
 
-class GramSchmidt3DScene(GramSchmidt3DSceneAssets):
+class GramSchmidt3DScene(GramSchmidt3DAssets):
   def construct(self):
-    # ---------------- Config ----------------
     self.camera.background_color=self.BG_COLOR
 
+    self.set_camera_orientation(
+      phi=70*DEGREES,
+      theta=45*DEGREES
+    )
 
 
-    # ---------------- Start Buff ----------------
+
+    # ---------------- Planes ----------------
+    xy_plane=self.build_xy_plane()
+    xz_plane=self.build_xz_plane()
+
+    self.play(
+      Create(xy_plane),
+      Create(xz_plane)
+    )
+
+
+
+    # ---------------- Point arrows ----------------
+    v1_point=self.build_point_arrow(self.VECTOR_COLOR)
+    v2_point=self.build_point_arrow(self.VECTOR_COLOR)
+    v3_point=self.build_point_arrow(self.VECTOR_COLOR)
+
+    self.add(v1_point,v2_point,v3_point)
+
+
+
+    # ---------------- Full arrows ----------------
+    v1_full=self.build_arrow(self.v1,self.VECTOR_COLOR)
+    v2_full=self.build_arrow(self.v2,self.VECTOR_COLOR)
+    v3_full=self.build_arrow(self.v3,self.VECTOR_COLOR)
+
+
+
+    # ---------------- Grow via Transform ----------------
+    self.play(
+      Transform(v1_point,v1_full),
+      Transform(v2_point,v2_full),
+      Transform(v3_point,v3_full)
+    )
+
+
+
     self.wait(1)
-
-
-
-    # ---------------- End Buff ----------------
-    self.wait(1)
-    
--- animations/src/scenes/gram_schmidt_3d/gram_schmidt_3d_scene_assets.py
+++ animations/src/scenes/gram_schmidt_3d/gram_schmidt_3d_scene_assets.py
@@ -1,17 +1,50 @@
 from manim import *
+import numpy as np
 
-class GramSchmidt3DSceneAssets(Scene):
+class GramSchmidt3DAssets(ThreeDScene):
   BG_COLOR=ManimColor('#1a1b26')
+  PLANE_COLOR=BLUE_E
+  SECOND_PLANE_COLOR=GREEN_E
+  VECTOR_COLOR=WHITE
 
-  def play(
-    self,
-    *args,
-    **kwargs
-  ):
-    if 'run_time' not in kwargs:
-      kwargs['run_time']=0.5
+  v1=np.array([3,1,0])
+  v2=np.array([1,3,0])
+  v3=np.array([4,4,3])
 
-    super().play(*args,**kwargs)
+  def build_xy_plane(self):
+    return NumberPlane(
+      x_range=[-6,6,1],
+      y_range=[-6,6,1],
+      background_line_style={
+        'stroke_opacity':0.3,
+        'stroke_color':self.PLANE_COLOR
+      }
+    )
 
-  def setup(self):
-    pass
+  def build_xz_plane(self):
+    plane=NumberPlane(
+      x_range=[-6,6,1],
+      y_range=[-6,6,1],
+      background_line_style={
+        'stroke_opacity':0.3,
+        'stroke_color':self.SECOND_PLANE_COLOR
+      }
+    )
+    plane.rotate(PI/2,axis=RIGHT)
+    return plane
+
+  def build_point_arrow(self,color):
+    return Arrow3D(
+      start=ORIGIN,
+      end=ORIGIN,
+      color=color,
+      thickness=0.02
+    )
+
+  def build_arrow(self,vec,color):
+    return Arrow3D(
+      start=ORIGIN,
+      end=vec,
+      color=color,
+      thickness=0.02
+    )
