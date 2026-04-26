b5929a1090fe1476200ef1ab5e3c51c45a0c7700
Adriano <adriytkr@gmail.com>
2026-04-26 16:10:34 -0300

    improve eigenvectors

--- src/scenes/eigenvectors/eigenvectors_scene.py
+++ src/scenes/eigenvectors/eigenvectors_scene.py
@@ -1,9 +1,12 @@
 from manim import *
-
+import numpy as np
 from src.scenes.eigenvectors.eigenvectors_scene_assets import EigenvectorsSceneAssets
 
+
 class EigenvectorsScene(EigenvectorsSceneAssets):
+
   def construct(self):
+
     # ---------------- Config ----------------
     self.camera.background_color=self.BG_COLOR
 
@@ -17,46 +20,113 @@ class EigenvectorsScene(EigenvectorsSceneAssets):
     el_non_eigen=self.build_vec(self.non_eigen_v)
     self.play(GrowArrow(el_non_eigen))
 
+    lbl_non_eigen=self.build_vec_label('u')\
+      .next_to(
+        el_non_eigen.get_end(),
+        DOWN*2,
+        buff=0.15
+      )
+    self.play(FadeIn(lbl_non_eigen))
+
     el_non_eigen_span=self.build_span(self.non_eigen_v)
-    self.play(FadeIn(el_non_eigen_span))
+    lbl_non_eigen_span=self.build_span_label('u')\
+      .next_to(
+        self.span_label_pos(self.non_eigen_v),
+        RIGHT+DOWN*0.2,
+        buff=0.15
+      )
+    self.play(
+      FadeIn(
+        el_non_eigen_span,
+        lbl_non_eigen_span
+      )
+    )
 
-    self.play(*self.mv(self.matrix,el_non_eigen))
+    self.play(
+      *self.mv(
+        self.matrix,el_non_eigen,
+        labels=[lbl_non_eigen]
+      )
+    )
 
-    self.play(FadeOut(el_non_eigen,el_non_eigen_span))
+    self.play(
+      FadeOut(
+        el_non_eigen,
+        lbl_non_eigen,
+        el_non_eigen_span,
+        lbl_non_eigen_span
+      )
+    )
 
     # ---------------- Eigenvector 1 ----------------
     el_eigen_v1=self.build_vec(self.eigen_v1)
     self.play(GrowArrow(el_eigen_v1))
 
-    el_v1_span=self.build_span(self.eigen_v1)
-    self.play(FadeIn(el_v1_span))
+    lbl_eigen_v1=self.build_vec_label('v')\
+      .next_to(
+        el_eigen_v1.get_end(),
+        DOWN*2,
+        buff=0.15
+      )
+    self.play(FadeIn(lbl_eigen_v1))
 
-    self.play(*self.mv(self.matrix,el_eigen_v1))
-    self.play(FadeOut(el_eigen_v1))
+    el_v1_span=self.build_span(self.eigen_v1)
+    lbl_v1_span=self.build_span_label('v')\
+      .next_to(
+        self.span_label_pos(self.eigen_v1),
+        DOWN*2,
+        buff=0.15
+      )
+    self.play(
+      FadeIn(
+        el_v1_span,
+        lbl_v1_span
+      ),
+    )
+
+    self.play(
+      *self.mv(
+        self.matrix,el_eigen_v1,
+        labels=[lbl_eigen_v1]
+      )
+    )
+
+    self.play(
+      FadeOut(
+        el_eigen_v1,
+        lbl_eigen_v1
+      )
+    )
 
     family=self.build_eigen_family(self.eigen_v1)
     self.play(GrowArrow(v) for v in family)
 
-    self.play(*self.mv(self.matrix,*family))
+    self.play(
+      *self.mv(
+        self.matrix,
+        *family
+      )
+    )
 
     self.remove(el_v1_span)
-    self.play(FadeOut(*family))
-
-    # ---------------- Eigenvector 2 ----------------
-    el_eigen_v2=self.build_vec(self.eigen_v2)
-    self.play(GrowArrow(el_eigen_v2))
-
-    el_v2_span=self.build_span(self.eigen_v2)
-    self.play(FadeIn(el_v2_span))
-
-    self.play(*self.mv(self.matrix,el_eigen_v2))
-    self.play(FadeOut(el_eigen_v2,el_v2_span))
+    self.play(
+      FadeOut(
+        *family,
+        lbl_v1_span
+      )
+    )
 
     # ---------------- No eigenvectors (rotation) ----------------
     no_eigen_family=self.build_circle_family()
     self.play(GrowArrow(v) for v in no_eigen_family)
 
-    self.play(*self.mv(self.rotation_matrix,*no_eigen_family))
+    self.play(
+      *self.mv(
+        self.rotation_matrix,
+        *no_eigen_family
+      )
+    )
+
     self.play(FadeOut(*no_eigen_family))
 
     # ---------------- End Buff ----------------
--- src/scenes/eigenvectors/eigenvectors_scene_assets.py
+++ src/scenes/eigenvectors/eigenvectors_scene_assets.py
@@ -1,10 +1,8 @@
 from manim import *
-
 import numpy as np
 
 class EigenvectorsSceneAssets(Scene):
   BG_COLOR=ManimColor('#1a1b26')
-
   VECTOR_COLOR=ManimColor('#00e676')
   SPAN_COLOR=ManimColor('#00e676')
 
@@ -12,7 +10,7 @@ class EigenvectorsSceneAssets(Scene):
     x_range=[-10,10],
     y_range=[-6,6],
     background_line_style={
-      "stroke_opacity":0
+      'stroke_opacity':0
     }
   )
 
@@ -42,6 +40,18 @@ class EigenvectorsSceneAssets(Scene):
 
     super().play(*args,**kwargs)
 
+  def _clamp_to_frame(self,u)->np.ndarray:
+    fx=config.frame_width/2
+    fy=config.frame_height/2
+
+    mx=abs(self.coords.p2c([fx,0,0])[0])
+    my=abs(self.coords.p2c([0,fy,0])[1])
+
+    tx=mx/abs(u[0]) if u[0]!=0 else np.inf
+    ty=my/abs(u[1]) if u[1]!=0 else np.inf
+
+    return u*min(tx,ty)
+
   def build_vec(self,vec)->Arrow:
     return Arrow(
       start=ORIGIN,
@@ -50,34 +60,63 @@ class EigenvectorsSceneAssets(Scene):
       color=self.VECTOR_COLOR
     ).set_z_index(999)
 
+  def build_vec_label(self,name:str)->MathTex:
+    return MathTex(
+      r'\vec{'+name+r'}',
+      color=self.VECTOR_COLOR
+    ).scale(0.8)
+
   def build_span(self,vec)->DashedLine:
     u=vec/np.linalg.norm(vec)
+    tip=self._clamp_to_frame(u)
 
     return DashedLine(
-      start=self.coords.c2p(*(u*10)),
-      end=self.coords.c2p(*(-u*10)),
+      start=self.coords.c2p(*tip),
+      end=self.coords.c2p(*(-tip)),
       color=self.SPAN_COLOR
     )
 
-  def mv(self,matrix,*vectors)->list:
+  def build_span_label(self,name:str)->MathTex:
+    return MathTex(
+      r'\text{span}('+name+r')',
+      color=self.SPAN_COLOR
+    ).scale(0.7)
+
+  def mv(
+    self,matrix,
+    *vectors,
+    labels=None
+  )->list:
     animations=[]
 
-    for el_v in vectors:
+    for i,el_v in enumerate(vectors):
       math_coords=self.coords.p2c(el_v.get_end())
       new_v=matrix@math_coords
+      new_end=self.coords.c2p(new_v[0],new_v[1])
 
       el_new_v=Arrow(
         start=ORIGIN,
-        end=self.coords.c2p(new_v[0],new_v[1]),
+        end=new_end,
         buff=0,
         color=self.VECTOR_COLOR
       )
 
       animations.append(Transform(el_v,el_new_v))
 
+      if labels is not None and i<len(labels) and labels[i] is not None:
+        lbl=labels[i]
+        offset=lbl.get_center()-el_v.get_end()
+        animations.append(
+          lbl.animate.move_to(new_end+offset)
+        )
+
     return animations
 
-  def build_eigen_family(self,eigen_v,scalar_range=range(-4,4))->list:
+  def build_eigen_family(
+    self,
+    eigen_v,
+    scalar_range=range(-4,4)
+  )->list:
     return [
       Arrow(
         start=ORIGIN,
@@ -98,3 +137,12 @@ class EigenvectorsSceneAssets(Scene):
       )
       for a in np.linspace(0,2*np.pi,n_vectors,endpoint=False)
     ]
+
+  def span_label_pos(self,vec)->np.ndarray:
+    u=vec/np.linalg.norm(vec)
+
+    tx=6.2/abs(u[0]) if u[0]!=0 else np.inf
+    ty=3.2/abs(u[1]) if u[1]!=0 else np.inf
+    t=min(tx,ty)
+
+    return self.coords.c2p(*(u*t))
